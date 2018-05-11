7ef9263acfbde3f91e5fad738fecfddecaf9e422
Airi Takahashi <libraloveli1011@gmail.com>
2018-05-11 17:55:40 +0900

    5-01まで終了（合ってるか確認してもらう）

--- Practice/src/practice04/PTra04_02.java
+++ Practice/src/practice04/PTra04_02.java
@@ -32,7 +32,7 @@ public class PTra04_02 {
 			}
 
 			// ★ 変数indexの値が3でも5でも割り切れない場合は、変数indexの値を出力
-			if(index%3!=0 ||index%5!=0) {
+			if(index%3!=0 && index%5!=0) {
 			   System.out.println(index);
 			}
 			// ★ 変数indexに1を加算した数を、変数indexに代入する
