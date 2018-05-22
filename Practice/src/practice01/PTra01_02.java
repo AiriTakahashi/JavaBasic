0e7a871ddb7636cb55a4d43bcbf6edcdc750ca0b
Airi Takahashi <libraloveli1011@gmail.com>
2018-05-22 17:54:57 +0900

    最初からやりなおし。3‐04まで終了

--- Practice/src/practice01/PTra01_02.java
+++ Practice/src/practice01/PTra01_02.java
@@ -8,14 +8,13 @@ package practice01;
 
 public class PTra01_02 {
 	public static void main(String[] args) {
-
 		// ★ 12～14行目をコメントにしてください。
-		ここをコメントにしてください
-		ここをコメントにしてください
-		ここをコメントにしてください
+		System.out.println("ここをコメントにしてください");
+		System.out.println("ここをコメントにしてください");
+		System.out.println("ここをコメントにしてください");
 
 		//  ★「Hello, world」と出力（コマンドプロンプトに表示）してください
-
+		System.out.println("Hello,world");
 
 	}
 }
--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -22,16 +22,25 @@ public class PTra01_07 {
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
 
-
-
-
-
-
-
-
+		bo=true;
+		by=1;
+		s=28;
+		c='L';
+		i=200000;
+		l=1000000000;
+		f=48.2f;
+		d=3.14;		
 
 		// ★ それぞれの変数の中身を出力してください
-
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		
 
 
 
